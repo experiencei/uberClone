1ccc9076f48c30cef7457cfc2c9224516779e6f2
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 20:49:03 +0100

    working on the google search and nearby

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -13,7 +13,29 @@ const NavigateCard = () => {
         <SafeAreaView style={tw`bg-white flex-1`}>
             <Text style={tw`text-center py-5 text-xl`}>Hey, User</Text>
             <View style={tw`border-t border-gray-200 flex-shrink`}>
-
+            <GooglePlacesAutocomplete
+                placeholder="Where to?"
+                styles={toInputBoxStyles}
+                fetchDetails={true}
+                returnKeyType={"search"}
+                minLength={2}
+                onPress={(data, details = null) => {
+                    dispatch(
+                    setDestination({
+                        location: details.geometry.location,
+                        description: data.description,
+                    })
+                    );
+                    navigation.navigate("RideOptionCard");
+                }}
+                enablePoweredByContainer={false}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: "en",
+                }}
+                nearbyPlacesAPI="GooglePlacesSearch"
+                debounce={400}
+        />
             </View>
         </SafeAreaView>
     )
