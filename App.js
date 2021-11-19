197153e9191d8fef031d79135ae1aed400d00587
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 17:18:31 +0100

    adding react navigation and implementation

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import { Provider } from "react-redux";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
-
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 export default function App() {
   return (
     <Provider store={store}>
+    <NavigationContainer>
      <SafeAreaProvider>
       <HomeScreen />
     </SafeAreaProvider>
+  </NavigationContainer>
     </Provider>
   );
 }
