47ea4eabd4880cea5c7bd76fc62b91613946bf92
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 20:21:58 +0100

    working on the google search and nearby

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Map from "../components/Map";
 
 const MapScreen = () => {
     return (
         <View>
-            <Text>Show me that</Text>
+            <Map />
         </View>
     )
 }
