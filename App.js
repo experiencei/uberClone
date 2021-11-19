73a3bd6e33fe5f3f61d0f06e50b9b38cb4729405
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 16:22:33 +0100

    working on the home screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 export default function App() {
