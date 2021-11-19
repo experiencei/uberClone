34f65ea43c28d9a0402b22f46fc737320b0719be
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 23:49:02 +0100

    homescreen and react-navigation finished

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,30 @@
-import React from 'react'
+//rnfes
+import React from "react";
 import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
 import { setDestination, setOrigin } from "../slices/navSlice";
-
-import NavOptions from "../components/NavOptions";
-import tw from "tailwind-react-native-classnames";
+import NavFavourites from "../components/NavFavourites";
 
 const HomeScreen = () => {
-    return (
-        <SafeAreaView style={tw`bg-white h-full`}>
-            <View style={tw`p-5`}>
-                <Image
-                    style={{
-                    width: 100,
-                    height: 100,
-                    resizeMode: "contain",
-                     }}
-                source={{
-                    uri: "https://links.papareact.com/gzs"
-                   }}
-                />
-            <GooglePlacesAutocomplete
+  const dispatch = useDispatch();
+  return (
+    <SafeAreaView style={tw`bg-white h-full pt-10 pl-5 `}>
+      <View>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <GooglePlacesAutocomplete
           placeholder="Where from?"
           styles={{
             container: {
@@ -52,12 +54,18 @@ const HomeScreen = () => {
           nearbyPlacesAPI="GooglePlacesSearch"
           debounce={400}
         />
-              <NavOptions/>
-            </View>
-        </SafeAreaView>
-    )
-}
+        <NavOptions />
+        <NavFavourites />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+    marginTop: "10px",
+  },
+});
