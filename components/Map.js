41eaf5a1b5ef0c3f777d25d54ac23e7afd8367cb
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 20:58:53 +0100

    working on the google search and nearby

--- components/Map.js
+++ components/Map.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
 import { useDispatch, useSelector } from "react-redux";
+import MapViewDirections from "react-native-maps-directions";
 
 
 const Map = () => {
@@ -19,6 +20,16 @@ const Map = () => {
             longitudeDelta: 0.005,
         }}
       >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+          lineDashPattern={[0]}
+        />
+      )}
           {origin?.location && (
         <Marker
           coordinate={{
