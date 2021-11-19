5059474e18247b908a189b51a72d4ee819c6ffbe
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 23:43:40 +0100

    working on map view and styling

--- components/Map.js
+++ components/Map.js
@@ -1,25 +1,56 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker } from "react-native-maps";
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import MapView, { Marker } from "react-native-maps";
+import {
+  selectDestination,
+  selectOrigin,
+  setTravelTimeInformation,
+} from "../slices/navSlice";
 import { useDispatch, useSelector } from "react-redux";
 import MapViewDirections from "react-native-maps-directions";
-
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useNavigation } from "@react-navigation/native";
 
 const Map = () => {
-    const origin = useSelector(selectOrigin);
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
 
-    return (
-        <MapView
-        style={tw`flex-1`}
-        mapType="mutedStandard"
-        initialRegion={{
-            latitude: origin.location.lat,
-            longitude: origin.location.lng,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
-        }}
-      >
+  useEffect(() => {
+    if (!origin || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 200, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    const getTravelTime = async () => {
+      {
+        const URL = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`;
+        const data = await fetch(URL).then((response) => response.json());
+        if (data.status !== "OK") return alert(data.error_message);
+        dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+      }
+    };
+
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_APIKEY]);
+
+  return (
+    <MapView
+      ref={mapRef}
+      style={tw`flex-1`}
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    >
       {origin && destination && (
         <MapViewDirections
           origin={origin.description}
@@ -30,7 +61,7 @@ const Map = () => {
           lineDashPattern={[0]}
         />
       )}
-          {origin?.location && (
+      {origin?.location && (
         <Marker
           coordinate={{
             latitude: origin.location.lat,
@@ -41,10 +72,21 @@ const Map = () => {
           identifier="origin"
         />
       )}
-      </MapView>
-    )
-}
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
+        />
+      )}
+    </MapView>
+  );
+};
 
-export default Map
+export default Map;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
