fa5151dbb49d6839f61f2c2726f058b6065c07ce
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 16:50:47 +0100

    working on the home screen

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
-    <View style={styles.container}>
+     <SafeAreaProvider>
       <HomeScreen />
-    </View>
+    </SafeAreaProvider>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
