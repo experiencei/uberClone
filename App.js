67e89c16e1154b6ce327a1ba1f3bc6d30004e654
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 17:21:42 +0100

    adding react navigation and implementation

--- App.js
+++ App.js
@@ -7,12 +7,23 @@ import { store } from "./store";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
     <NavigationContainer>
      <SafeAreaProvider>
-      <HomeScreen />
+     <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+       </Stack.Navigator>
     </SafeAreaProvider>
   </NavigationContainer>
     </Provider>
