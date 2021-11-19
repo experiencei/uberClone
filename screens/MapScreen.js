50a862e39e173e528ff57c8775612ab1a11b2092
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 23:50:05 +0100

    integration of the react native map view for mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,42 +1,49 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Map from "../components/Map";
-import tw from "tailwind-react-native-classnames";
+import { useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import NavigateCard from '../components/NavigateCard';
-import RideOptionCard from '../components/RideOptionCard';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Icon } from "react-native-elements/dist/icons/Icon";
+import tw from "tailwind-react-native-classnames";
+import Map from "../components/Map";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionCard from "../components/RideOptionCard";
 
 const MapScreen = () => {
   const Stack = createStackNavigator();
-
-    return (
-        <View>
-            <View style={tw`h-1/2`}>
-                <Map />
-            </View>
-            <View style={tw`h-1/2`}>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="NavigateCard"
-                    component={NavigateCard}
-                    options={{
-                    headerShown: false,
-                    }}
-                />
-                <Stack.Screen
-                    name="RideOptionCard"
-                    component={RideOptionCard}
-                    options={{
-                    headerShown: false,
-                    }}
-                />
+  const navigation = useNavigation();
+  return (
+    <View>
+      <TouchableOpacity
+        style={tw`bg-gray-100 absolute top-16 left-8 z-10 p-3 rounded-full shadow-lg`}
+        onPress={() => navigation.navigate("HomeScreen")}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="RideOptionCard"
+            component={RideOptionCard}
+            options={{
+              headerShown: false,
+            }}
+          />
         </Stack.Navigator>
-            </View>
-           
-        </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
-export default MapScreen
+export default MapScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
