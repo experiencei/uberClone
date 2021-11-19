6df3c619a5280813b15ddd5506b3bb406f0a1395
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 16:11:45 +0100

    slice , store and reducer

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
