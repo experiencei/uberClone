cd779d1bfe20394e42d185397eb9fee67270c473
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 17:20:03 +0100

    adding react navigation and implementation

--- App.js
+++ App.js
@@ -20,3 +20,8 @@ export default function App() {
 }
 
 
+
+
+
+
+// expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
