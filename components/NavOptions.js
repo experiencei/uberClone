d85a268eb84671ecc14dc4265372e4e5a13cb66b
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 16:35:18 +0100

    working on the home screen

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+
+const data = [
+    {
+      id: "123",
+      title: "Get a rid",
+      image: "https://links.papareact.com/3pn",
+      screen: "MapScreen",
+    },
+    {
+      id: "456",
+      title: "Order food",
+      image: "https://links.papareact.com/28w",
+      screen: "EatsScreen",
+    },
+  ];
+
 const NavOptions = () => {
     return (
         <View>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -18,6 +18,7 @@ const HomeScreen = () => {
                    }}
                 />
             </View>
+            <NavOptions/>
         </SafeAreaView>
     )
 }
