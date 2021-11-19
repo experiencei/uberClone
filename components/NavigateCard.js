d3d660ee73a3efe2599d7bdf0d87fee0df1f9f1d
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 20:48:38 +0100

    working on the google search and nearby

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -5,11 +5,16 @@ import {SafeAreaView,
     View,
     StyleSheet} from 'react-native'
 import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const NavigateCard = () => {
     return (
         <SafeAreaView style={tw`bg-white flex-1`}>
             <Text style={tw`text-center py-5 text-xl`}>Hey, User</Text>
+            <View style={tw`border-t border-gray-200 flex-shrink`}>
+
+            </View>
         </SafeAreaView>
     )
 }
