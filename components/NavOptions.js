b4d4cd0ee11e0367931f69c61da0de2e41cd855e
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 23:47:04 +0100

    navoption for homescreen

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,55 +1,51 @@
-import React from 'react'
-import {FlatList, Text, TouchableOpacity, View, Image } from 'react-native'
-import tw from "tailwind-react-native-classnames";
+import React from "react";
+import { FlatList, Text, TouchableOpacity, View, Image } from "react-native";
 import { Icon } from "react-native-elements";
+import { useSelector } from "react-redux";
+import tw from "tailwind-react-native-classnames";
 import { useNavigation } from "@react-navigation/native";
-import { useSelector } from 'react-redux';
-import { selectOrigin } from '../slices/navSlice';
-
-
-
+import { selectOrigin } from "../slices/navSlice";
 
 const data = [
-    {
-      id: "123",
-      title: "Get a rid",
-      image: "https://links.papareact.com/3pn",
-      screen: "MapScreen",
-    },
-    {
-      id: "456",
-      title: "Order food",
-      image: "https://links.papareact.com/28w",
-      screen: "EatsScreen",
-    },
-  ];
+  {
+    id: "123",
+    title: "Get a rid",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
 
 const NavOptions = () => {
   const navigation = useNavigation();
   const origin = useSelector(selectOrigin);
-
-    return (
-        <FlatList
+  return (
+    <FlatList
       data={data}
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => (
         <TouchableOpacity
-        onPress={() => navigation.navigate(item.screen)}
-        style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 rounded-2xl`}
-        disabled={!origin}
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 rounded-2xl`}
+          disabled={!origin}
         >
-            <View>
-                <Image 
-                     style={{
+          <View style={tw`${!origin && "opacity-20"}`}>
+            <Image
+              style={{
                 width: 120,
                 height: 120,
                 resizeMode: "contain",
               }}
-                    source={{
+              source={{
                 uri: item.image,
               }}
-                />
+            />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
             <Icon
               style={tw`p-2 bg-black rounded-full  w-10 mt-4  `}
@@ -57,11 +53,11 @@ const NavOptions = () => {
               color="white"
               type="antdesign"
             />
-            </View>
+          </View>
         </TouchableOpacity>
       )}
-          />
-    )
-}
+    />
+  );
+};
 
 export default NavOptions;
