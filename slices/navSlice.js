835157bf18ac424ee6d5aebe4fefa3dbfb1b6355
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 16:14:35 +0100

    slice , store and reducer

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -26,3 +26,12 @@ export const navSlice = createSlice({
 // Action creators are generated for each case reducer function
 export const { setOrigin, setDestination, setTravelTimeInformation } =
   navSlice.actions;
+
+
+  //Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
